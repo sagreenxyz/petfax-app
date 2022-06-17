379f5b7ec495800bb3b22e7075a277a6ed4b5dde
Scott A. Green <sagreenxyz@gmail.com>
2022-06-17 17:39:08 -0500

    rearranging templates for pets into separate folder, preparing for facts template in separate folder

--- petfax/pet.py
+++ petfax/pet.py
@@ -8,10 +8,10 @@ bp = Blueprint('pet', __name__, url_prefix="/pets")
 # pet index route
 @bp.route('/')
 def index(): 
-    return render_template('index.html', pets=pets)
+    return render_template('pets/index.html', pets=pets)
 
 # pet detail
 @bp.route('/<int:id>')
 def show(id):
     pet = pets[id - 1]
-    return render_template('show.html', pet=pet)
+    return render_template('pets/show.html', pet=pet)
