b7766ff25ed3c459a488b37249533d4b8650a75e
Scott A. Green <sagreenxyz@gmail.com>
2022-06-17 17:31:21 -0500

    creating route to render show.html for specified pet

--- petfax/pet.py
+++ petfax/pet.py
@@ -5,6 +5,13 @@ pets = json.load(open('pets.json'))
 
 bp = Blueprint('pet', __name__, url_prefix="/pets")
 
+# pet index route
 @bp.route('/')
 def index(): 
     return render_template('index.html', pets=pets)
+
+# pet detail
+@bp.route('/<int:id>')
+def show(id):
+    pet = pets[id - 1]
+    return render_template('show.html', pet=pet)
