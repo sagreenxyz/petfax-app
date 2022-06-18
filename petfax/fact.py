228b0d3052a4331cbb90bb78bfe1457bfa01094c
Scott A. Green <sagreenxyz@gmail.com>
2022-06-17 21:39:57 -0500

    setting up variables to capture request.form submitted values

--- petfax/fact.py
+++ petfax/fact.py
@@ -6,7 +6,9 @@ bp = Blueprint('fact', __name__, url_prefix="/facts")
 @bp.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        print(request.form)
+        submitter = request.form['submitter']
+        fact = request.form['fact']
+        
         return redirect('/facts')
 
     return render_template('facts/index.html')
