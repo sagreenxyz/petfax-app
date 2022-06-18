95c00072d4885dd5774a5ecbda1a198d1090751d
Scott A. Green <sagreenxyz@gmail.com>
2022-06-17 21:28:14 -0500

    configuring SQLAlchemy

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -3,6 +3,9 @@ from flask import Flask
 def create_app(): 
     app = Flask(__name__)
 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@127.0.0.1:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False     
+
     @app.route('/')
     def hello(): 
         return 'Hello, PetFax!'
